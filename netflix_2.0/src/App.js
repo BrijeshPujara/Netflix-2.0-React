f570197c541a60b11748da5ea1b56fbf9e785d69
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-10-13 00:10:32 +0100

    working on signup and login

--- netflix_2.0/src/App.js
+++ netflix_2.0/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import "./App.css";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </Router>
-      );
     </div>
   );
 }
---
+++ netflix_2.0/src/screens/SignInScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignInScreen() {
+  return (
+      <div className='signInScreen'>
+          
+    </div>
+  )
+}
+
+export default SignInScreen
