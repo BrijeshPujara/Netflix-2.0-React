f18400b0b3c1c8b54222b5e9fc8d9f45ae29be74
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-10-13 13:06:22 +0100

    signup authentication wip

--- netflix_2.0/src/screens/SignInScreen.js
+++ netflix_2.0/src/screens/SignInScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./SignInScreen.css";
 
 function SignInScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
   return (
-      <div className='signInScreen'>
-          
+    <div className="signInScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit" onClick={signIn}>
+          {" "}
+          Sign In
+        </button>
+        <h4>
+          <span className="signInScreen__gray"> New to Netflix?</span>
+          <span className="signInScreen__link" onClick={register}>Sign up now.</span>
+        </h4>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignInScreen
+export default SignInScreen;
