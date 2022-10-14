385f2ebfe8c6041a607a81e8c919d36d52906993
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-10-14 19:45:26 +0100

    updated reducer and login created

--- netflix_2.0/src/App.js
+++ netflix_2.0/src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import "./App.css";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  },[]);
 
   return (
     <div className="app">
